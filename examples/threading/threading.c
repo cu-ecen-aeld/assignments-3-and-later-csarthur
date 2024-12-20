1387f22defe517c60e2209a87cd0066169818ef7
Chris Arthur <chris.arthur@adtran.com>
2024-12-20 10:18:39 -0600

    Checkin for assignment 4 part 1; finder-app makefile changes in prep for part 2

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -9,25 +9,41 @@
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
-{
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+{   
+    struct thread_data * thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = false;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0)
+    {        
+        return thread_param;
+    }
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    if (pthread_mutex_unlock(thread_func_args->mutex) == 0)
+    {
+        thread_func_args->thread_complete_success = true;
+    }
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data * the_thread_data = malloc(sizeof(struct thread_data));
+
+    if (the_thread_data == NULL)
+    {
+        return false;
+    }
+
+    the_thread_data->mutex = mutex;
+    the_thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    the_thread_data->wait_to_release_ms = wait_to_release_ms;
+
+    int rc = pthread_create(thread, NULL, threadfunc, (void *)the_thread_data);
+    if (rc != 0)
+    {
+        return false;
+    }
+    return true;
 }
 
